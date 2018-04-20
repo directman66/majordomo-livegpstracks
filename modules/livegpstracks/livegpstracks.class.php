af9112494ecd4f08bc7ad6fdd4a5623157e63839
directman66 <sannikovdi@yandex.ru>
2018-04-20 18:54:52 +0500

    Update livegpstracks.class.php

--- modules/livegpstracks/livegpstracks.class.php
+++ modules/livegpstracks/livegpstracks.class.php
@@ -530,6 +530,7 @@ $prop_id=addClassProperty('livegpstracks', 't', 10);
  lgps_in: DID varchar(30) NOT NULL
  lgps_in: TITLE varchar(100) NOT NULL DEFAULT ''
  lgps_in: VALUE varchar(100)
+ lgps_in: STREET varchar(100)
  lgps_in: UPDATED datetime
  lgps_in: LINKED_OBJECT varchar(100) NOT NULL DEFAULT ''
  lgps_in: LINKED_PROPERTY varchar(100) NOT NULL DEFAULT ''
