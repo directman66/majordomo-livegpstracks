c8a5edeb06a05d1596c5068d5da3e967bd6e7ebc
directman66 <sannikovdi@yandex.ru>
2018-05-12 19:55:58 +0500

    Update cycle_livegpstracks.php

--- scripts/cycle_livegpstracks.php
+++ scripts/cycle_livegpstracks.php
@@ -21,7 +21,7 @@ if ((!$tmp1['ID']) && (!$tmp2['ID']))
  
 echo date("H:i:s") . " running " . basename(__FILE__) . PHP_EOL;
 $latest_check=0;
-$checkEvery=300; // poll every 5 minutesz
+$checkEvery=60; // poll every 5 minutesz
 while (1)
 {
    setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
